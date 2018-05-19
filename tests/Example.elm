92c24378a330dc9d667b20c39792e046dd5ece1d
omersagy <omersagyemails@gmail.com>
2018-05-18 22:52:07 -0400

    add test cases for zip and enumerate

--- tests/Example.elm
+++ tests/Example.elm
@@ -4,7 +4,24 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
+import Annex
+
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+
+  describe "Basic Functionality"
+  [
+
+    test "does not crash" 
+      (\_ -> Expect.equal 4 (2 + 2))
+
+  , test "zip works" 
+      (\_ -> Expect.equal (Annex.zip [1, 2] ["a", "b"])
+                          [(1, "a"), (2, "b")])
+
+  , test "enumerate works"
+      (\_ -> Expect.equal (Annex.enumerate ["a", "b", "c"])
+                          [(0, "a"), (1, "b"), (2, "c")])
+  ]
+
