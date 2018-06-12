a71677ecc555838f5a741e661142eca6a14a9469
omersagy <omersagyemails@gmail.com>
2018-06-12 01:43:33 -0400

    flesh out game model

--- Game/Model.elm
+++ Game/Model.elm
@@ -1,11 +1,45 @@
 module Game.Model exposing(..)
 
+import Time exposing(Time)
 
-type alias Model = { dummy: Int }
 
+initialModel : Model
+initialModel = 
+  { gameTime = 0 
+  , messageHistory = []
+  , eventQueue = []
+  , storyEventCorpus = []
+  }
+
+
+type alias Model = 
+
+  -- Time passed in the game so far.
+  { gameTime : Time
+
+  -- Messages to be displayed on-screen.
+  , messageHistory : List String 
+
+  -- Events waiting to be executed.
+  , eventQueue : List Event
+
+  -- All story events that could be triggered.
+  , storyEventCorpus : List Event
+
+  }
+
+type Event = DisplayText String
+             | DisplayChoice List String 
+
+type alias Choice =
+  { text : String
+  , consequence : Maybe Event
+  } 
+
+type alias StoryEvent = { text: List String }
+
+-- MESSAGES --
 
 type Message = DummyMessage
 
 
-initialModel : Model
-initialModel = { dummy = 2 }
