6fa27e72823aec8ab2f0277bbb523366d0fce926
omersagy <omersagyemails@gmail.com>
2018-06-13 18:42:05 -0400

    wrote first event

--- Game/Story.elm
+++ Game/Story.elm
@@ -1,5 +1,8 @@
 module Game.Story exposing(..)
 
+import List
+import Time exposing(Time)
+
 import Game.GameState exposing(..)
 import Game.Event exposing(Event)
 
@@ -8,7 +11,8 @@ type alias StoryEvent =
   { name: String
   , trigger: Trigger 
   , text: List String 
-  , choices: List Choice
+  , choices: Maybe (List Choice)
+  , occursOnce: Bool
   }
 
 
@@ -18,4 +22,27 @@ type alias Choice =
   } 
 
 
+triggeredStoryEvents : List StoryEvent -> GameState -> List StoryEvent
+triggeredStoryEvents events state =
+  List.filter (\e -> e.trigger state) events
+
+
+-- TRIGGERS 
+
 type alias Trigger = GameState -> Bool
+
+gameTimePassed : Time -> Trigger
+gameTimePassed t = (\s -> s.gameTime >= t)
+
+
+-- ACTUAL STORY EVENTS
+
+storyEventCorpus = 
+  [
+    { name = "begin"
+    , trigger = gameTimePassed (1 * Time.second)
+    , text = [ "hello world!" ]
+    , choices = Nothing 
+    , occursOnce = True
+    }
+  ]
