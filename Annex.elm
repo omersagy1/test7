1db64300f074c5f1f7f5e5ccbde783d389f2acd7
omersagy <omersagyemails@gmail.com>
2018-06-15 18:37:03 -0400

    annex spacing

--- Annex.elm
+++ Annex.elm
@@ -2,11 +2,16 @@ module Annex exposing (..)
 
 import  List
 
+-- This module is for generic utility functions
+-- not found in the Elm standard library.
+
 zip : List a -> List b -> List (a, b)
 zip = List.map2 (,)
 
+
 enumerate: List a -> List (Int, a)
 enumerate list = zip (List.range 0 (List.length list)) list
+
 
 -- Given a function guarateed to produce a value
 -- and an argument that might exist, produce a
@@ -18,14 +23,17 @@ maybeChain callback maybe =
     Just value ->
       Just (callback value)
 
+
 ignoreResult : (s -> (a, s)) -> (s -> s)
 ignoreResult f = (\x -> (f x) |> Tuple.second)
+
 
 isJust : Maybe a -> Bool
 isJust m =
   case m of
     Nothing -> False
     (Just x) -> True
+
 
 maybeToList : Maybe a -> List a
 maybeToList m =
