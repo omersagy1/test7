98806bdf7561780b577db570905312b8ad334f89
omersagy <omersagyemails@gmail.com>
2018-07-08 13:20:42 -0400

    more structure to creating meters.

--- src/Render/Meter.elm
+++ src/Render/Meter.elm
@@ -7,6 +7,54 @@ import Html.Styled.Attributes exposing (style, value, css)
 
 import Render.Constants as Constants
 
+type Meter = Meter Params
+
+type alias Params =
+  { width : Px
+  , height : Px
+  , background : Color
+  , barColorFn : Meter -> Color
+  , fractionFilled : Float
+  , active : Bool
+  , label : String
+  , borderColorFn : Meter -> Color
+  }
+
+new : Meter
+new = Meter
+  { width = px 200
+  , height = px 50
+  , background = Colors.black
+  , barColorFn = (\_ -> Colors.gray)
+  , fractionFilled = 1.0
+  , active = True
+  , label = "default"
+  , borderColorFn = (\_ -> Colors.yellow)
+  }
+
+setBorderColorFn : (Meter -> Color) -> Meter -> Meter
+setBorderColorFn fn (Meter params) =
+  Meter { params | borderColorFn = fn }
+
+
+colorBorderOnActive : Color -> Color -> Meter -> Meter
+colorBorderOnActive inactiveColor activeColor (Meter params) =
+  setBorderColorFn 
+    (\(Meter params) -> if params.active then activeColor
+                        else inactiveColor)
+    (Meter params)
+
+
+fixedBarColor : Color -> Meter -> Meter
+fixedBarColor c (Meter params) =
+  Meter { params | barColorFn = (\_ -> c) }
+
+
+-- Interpolate between two colors based on the fraction
+-- of the bar that is filled.
+colorBarOnFraction : Color -> Color -> Meter -> Meter
+colorBarOnFraction emptyColor fullColor (Meter params) = Meter params
+
 
 margin : Float
 margin = 12
